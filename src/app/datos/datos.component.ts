d5398c54dfe3c23bbcf9c768df0a6a9974480297
Urrix <mauribarrien@gmail.com>
2023-11-27 17:57:01 -0600

    Come Back To Me

---
+++ src/app/datos/datos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-datos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './datos.component.html',
+  styleUrl: './datos.component.css'
+})
+export class DatosComponent {
+
+}
---
+++ src/app/elementos/elementos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-elementos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './elementos.component.html',
+  styleUrl: './elementos.component.css'
+})
+export class ElementosComponent {
+
+}
